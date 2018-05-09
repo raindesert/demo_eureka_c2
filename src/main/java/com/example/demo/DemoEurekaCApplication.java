03e367f1be04eb52fd954d7c5df1ab23e16c82c2
liruhao <liruhao@zhongan.com>
2018-05-09 15:11:07 +0800

    add

---
+++ src/main/java/com/example/demo/DemoEurekaCApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class DemoEurekaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaCApplication.class, args);
+	}
+	
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+    
+    @RequestMapping("/hello")
+    public String helloTest(@RequestParam String name) {
+        return "hello "+name+",i am from port:" +port;
+    }
+}
